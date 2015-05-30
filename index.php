ff84d3471b7cf0f897b62c1ce22722779c31a0ed
sarahmehmedi <sarahmehmedi@gmail.com>
2015-05-30 11:35:23 -0500

    Added about me and connect

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 <html>
 <head>
-    <title> Sarah Mehmedi </title>
+    <title > Sarah Mehmedi </title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-	<h1 class="red"> Sarah Mehmedi </h1>
-	<h2> About </h2>
-	<p>I am Sarah Mehmedi studying at LUC.</p>
-	<table>
-		<tr>
-		<td class="red">Jill </td>
-		<td id="blue">Smith </td>
-		</tr>
-		<tr>
-		<td> Eve </td>
-		<td class="red">Jackson</td>
-		</tr>
-	</table>
+	<h1> Sarah Mehmedi </h1>
+	<h2 class="yellow"> About </h2>
+	<p>Hi! My name is Sarah Mehmedi and I am currently studying Computer Science at Loyola University in Chicago. My interests include development of the front end because it is great to see the progress. I strive to make everything functional and without any imperfections.</p>
+
     <a href="http://www.google.com" target="_blank">GOOGLE</a>
     <img src="self.jpg" alt="Sarah" id="pic">
+
+    <h2 class="yellow">Connect With Me!</h2>
+    <table>
+        <tr>
+            <td >Email: </td>
+            <td >sarahmehmedi@gmail.com </td>
+        </tr>
+        <tr>
+            <td> Phone: </td>
+            <td >1(630)826-9266</td>
+        </tr>
+    </table>
+    <a href="https://www.linkedin.com/pub/sarah-mehmedi/79/482/5b7" target= "_blank" style="text-decoration:none;"><span style="font: 100% Arial,sans-serif; color:#0783B6;"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png" width="20" height="15" alt="View Sarah Mehmedi's LinkedIn profile" style="vertical-align:middle;" border="0">&nbsp;View My Profile</span></a>
+
 </body>
 </html>
