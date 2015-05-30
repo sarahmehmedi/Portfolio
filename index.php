cce54baed572cefcf5ee0e14a0330c29f2322c37
sarahmehmedi <sarahmehmedi@gmail.com>
2015-05-30 00:29:58 -0500

    Added style.css

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <html>
 <head>
-	<title>
-	Sarah Mehmedi
-	</title>
+    <title> Sarah Mehmedi </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<h1>Sarah Mehmedi </h1>
+	<h1 class="red"> Sarah Mehmedi </h1>
+	<h2> About </h2>
+	<p>I am Sarah Mehmedi studying at LUC.</p>
+	<table>
+		<tr>
+		<td class="red">Jill </td>
+		<td id="blue">Smith </td>
+		</tr>
+		<tr>
+		<td> Eve </td>
+		<td class="red">Jackson</td>
+		</tr>
+	</table>
+    <a href="http://www.google.com" target="_blank">GOOGLE</a>
+    <img src="self.jpg" alt="Sarah" id="pic">
 </body>
 </html>
