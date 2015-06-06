21c9a0e94a880b3cdadf6981f51b405bf410683e
sarahmehmedi <sarahmehmedi@gmail.com>
2015-06-05 19:51:52 -0500

    Updated with tabs

--- index.php
+++ index.php
@@ -1,31 +1,136 @@
 <html>
-<head>
-    <title > Sarah Mehmedi </title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<h1> Sarah Mehmedi </h1>
-	<h2 class="yellow"> About </h2>
-	<p>Hi! My name is Sarah Mehmedi and I am currently studying Computer Science at Loyola University in Chicago. My interests include development of the front end because it is great to see the progress and developing applications. I strive to make everything functional by fixing all of the imperfections.</p>
-    <h2 class="yellow">
-        <a href="resume.pdf" class="yellow" target="_blank">Click For Resume</a>
-    </h2>
 
-    <img src="self.jpg" alt="Sarah" id="pic">
+	<h1 class="yellow"> Sarah Mehmedi </h1>
+    <head>
+        <title> Sarah Mehmedi </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta charset="utf-8">
+        <title>jQuery UI Tabs - Default functionality</title>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <script>
+            $(function() {
+                $( "#tabs" ).tabs();
+            });
+        </script>
+
+    </head>
+    <body>
+
+    <div id="tabs">
+        <ul>
+            <li><a href="#tabs-1">About</a></li>
+            <li><a href="#tabs-2">Projects</a></li>
+            <li><a href="#tabs-3">Resume</a></li>
+            <li><a href="#tabs-4">Contact</a></li>
+        </ul>
+        <div id="tabs-1">
+            <p>Hi! My name is Sarah Mehmedi and I am currently studying Computer Science at Loyola University in Chicago. My interests include development of the front end because it is great to see the progress and developing applications. I strive to make everything functional by fixing all of the imperfections.</p>
+            <img src="self.jpg" alt="Sarah" id="pic">
 
 
-    <h2 class="yellow">Contact Information</h2>
-    <table>
-        <tr>
-            <td >Email: </td>
-            <td >sarahmehmedi@gmail.com </td>
-        </tr>
-        <tr>
-            <td> Phone: </td>
-            <td >1(630)826-9266</td>
-        </tr>
-    </table>
-    <a href="https://www.linkedin.com/pub/sarah-mehmedi/79/482/5b7" target= "_blank" style="text-decoration:none;"><span style="font: 100% Arial,sans-serif; color:#0783B6;"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png" width="20" height="15" alt="View Sarah Mehmedi's LinkedIn profile" style="vertical-align:middle;" border="0">&nbsp;View My Profile</span></a>
+            <a href="https://www.linkedin.com/pub/sarah-mehmedi/79/482/5b7" target= "_blank" style="text-decoration:none;"><span style="font: 100% Arial,sans-serif; color:#0783B6;"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png" width="20" height="15" alt="View Sarah Mehmedi's LinkedIn profile" style="vertical-align:middle;" border="0">&nbsp;View My Profile</span></a>
 
+        </div>
+        <div id="tabs-2">
+            <h2 class="yellow">Projects</h2>
+            <p>Projects Tab Under Construction</p>        </div>
+        <div id="tabs-3">
+            <a href="resume.pdf" class="yellow" target="_blank">Click For Resume</a>
+
+        </div>
+        <div id="tabs-4">
+
+            <h2 class="yellow">Contact Information</h2>
+            <table>
+                <tr>
+                    <td >Email: </td>
+                    <td >sarahmehmedi@gmail.com </td>
+                </tr>
+                <tr>
+                    <td> Phone: </td>
+                    <td >1(630)826-9266</td>
+                </tr>
+            </table>
+
+        </div>
+    </div>
+
+<!--
+    <div class="tabs">
+        <ul class="tab-links">
+            <li class="active"><a href="#tab1">About</a></li>
+            <li><a href="#tab2">Projects</a></li>
+            <li><a href="#tab3">Resume</a></li>
+            <li><a href="#tab4">Contact</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div id="tab1" class="tab active">
+                <p>Hi! My name is Sarah Mehmedi and I am currently studying Computer Science at Loyola University in Chicago. My interests include development of the front end because it is great to see the progress and developing applications. I strive to make everything functional by fixing all of the imperfections.</p>
+                <img src="self.jpg" alt="Sarah" id="pic">
+
+
+                <a href="https://www.linkedin.com/pub/sarah-mehmedi/79/482/5b7" target= "_blank" style="text-decoration:none;"><span style="font: 100% Arial,sans-serif; color:#0783B6;"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png" width="20" height="15" alt="View Sarah Mehmedi's LinkedIn profile" style="vertical-align:middle;" border="0">&nbsp;View My Profile</span></a>
+
+            </div>
+
+            <div id="tab2" class="tab">
+                <h2 class="yellow">Projects</h2>
+                <p>Projects Tab Under Construction</p>
+            </div>
+
+            <div id="tab3" class="tab">
+                <a href="resume.pdf" class="yellow" target="_blank">Click For Resume</a>
+            </div>
+
+            <div id="tab4" class="tab">
+                <h2 class="yellow">Contact Information</h2>
+                <table>
+                    <tr>
+                        <td >Email: </td>
+                        <td >sarahmehmedi@gmail.com </td>
+                    </tr>
+                    <tr>
+                        <td> Phone: </td>
+                        <td >1(630)826-9266</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
+
+<title > Sarah Mehmedi </title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1> Sarah Mehmedi </h1>
+<h2 class="yellow"> About </h2>
+<p>Hi! My name is Sarah Mehmedi and I am currently studying Computer Science at Loyola University in Chicago. My interests include development of the front end because it is great to see the progress and developing applications. I strive to make everything functional by fixing all of the imperfections.</p>
+<h2 class="yellow">
+    <a href="resume.pdf" class="yellow" target="_blank">Click For Resume</a>
+</h2>
+
+
+<img src="self.jpg" alt="Sarah" id="pic">
+
+
+<h2 class="yellow">Contact Information</h2>
+<table>
+    <tr>
+        <td >Email: </td>
+        <td >sarahmehmedi@gmail.com </td>
+    </tr>
+    <tr>
+        <td> Phone: </td>
+        <td >1(630)826-9266</td>
+    </tr>
+</table>
+<a href="https://www.linkedin.com/pub/sarah-mehmedi/79/482/5b7" target= "_blank" style="text-decoration:none;"><span style="font: 100% Arial,sans-serif; color:#0783B6;"><img src="https://static.licdn.com/scds/common/u/img/webpromo/btn_in_20x15.png" width="20" height="15" alt="View Sarah Mehmedi's LinkedIn profile" style="vertical-align:middle;" border="0">&nbsp;View My Profile</span></a>
+
+
+-->
