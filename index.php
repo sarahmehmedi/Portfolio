e4370620d2013f60e7e96e4f63fc69cd1adab21d
sarahmehmedi <sarahmehmedi@gmail.com>
2015-06-29 22:16:32 -0500

    fix

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="style.css">
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link rel="stylesheet" href="bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <script src="bootstrap.min.js"></script>
 
     </head>
     <body bgcolor="#E6E6FA">
